950d05e5e50e52c21a7aae88c49deb12794eb6f2
bernat <elbernat@gmail.com>
2019-11-03 17:06:06 -0500

    code update

--- README.md
+++ README.md
@@ -21,3 +21,6 @@ SketchRNN is a recurrent neural network model trained on millions of doodles col
 This original paper and implementation of SketchRNN was made in TensorFlow and ported to Magenta.js by David Ha. The ml5.js implementation was ported by Reiichiro Nakano.
 
 The ml5 library includes a list of supported SketchRNN models.
+
+## List of RNN Models
+[MODELS LIST](https://github.com/ml5js/ml5-library/blob/master/src/SketchRNN/models.js)
--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,41 @@
+let sketchrnn;
+let currentStroke;
+let x,y;
+let nextPen;
+
+function preload() {
+  sketchrnn = ml5.sketchRNN('cat');
+}
+
 function setup() {
   createCanvas(400, 400);
+  background(220);
+  x = width/2;
+  y = height/2;
+  sketchrnn.generate(gotStrokePath);
+  console.log('model loaded');
+}
+
+function gotStrokePath(error, strokePath) {
+  console.log(strokePath);
+  currentStroke = strokePath;
 }
 
 function draw() {
-  background(220);
+  if (currentStroke) {
+    stroke(0);
+    strokeWeight(3);
+    if(nextPen == 'end'){
+      noLoop();
+      return;
+    }
+    if(nextPen == 'down'){
+      line(x,y, x+currentStroke.dx, y+currentStroke.dy);
+    }
+    x += currentStroke.dx;
+    y += currentStroke.dy;
+    nextPen = currentStroke.pen;
+    currentStroke = null;
+    sketchrnn.generate(gotStrokePath);
+  }
 }
